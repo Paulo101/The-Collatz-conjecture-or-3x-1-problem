df68d0c31367463838c79f87f2f4b130ad08a020
Paulo NGOVE <paulon@bbd.co.za>
2022-01-20 09:55:33 +0200

    The Collatz conjecture or 3x + 1 problem solved in dotnet

---
+++ ConsoleApp2/Collatz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Collatz
+    {
+        public static List<int> Collazt(int n)
+        {
+            List<int> data = new List<int>();
+            data.Add(n);
+
+            int resul = 0;
+
+            while (true)
+            {
+
+                if (n == 1)
+                {
+                    break;
+                }
+                if ((n % 2) == 0)
+                {
+                    resul = n / 2;
+                    n = resul;
+
+                }
+                else
+                {
+                    resul = (n * 3) + 1;
+                    n = resul;
+                }
+                if (n % 2 != 0)
+                    data.Add(n);
+            }
+
+            return data;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+using ConsoleApp2;
+using System;
+
+namespace ConsoleApp2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("N: ");
+            string s = Console.ReadLine();
+            if(string.IsNullOrEmpty(s))
+                Main(args);
+            int r = int.Parse(s);
+            List<int> list = Collatz.Collazt(r);
+
+            Print(list);
+        }
+        static void Print(List<int> _list)
+        {
+            foreach (var item in _list)
+                Console.WriteLine(item);
+        }
+    }
+}
